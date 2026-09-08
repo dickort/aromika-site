a9508f04ab06ede106e8c3142c2b9a59f7a25c0c
dickort <yankovskiy92@gmail.com>
2026-09-08 10:53:13 +0500

    Add AROMIKA.INFO admin menu

---
+++ cs-cart-aromika-info-admin/app/addons/aromika_info_admin/schemas/menu/menu.post.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('BOOTSTRAP') or die('Access denied');
+
+$schema['central']['website']['items']['aromika_info_admin'] = array(
+    'position' => 980,
+    'title' => 'AROMIKA.INFO',
+    'href' => 'aromika_info.dashboard',
+);
+
+return $schema;
